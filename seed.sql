8e6b82ea056f1521dd9a30232d2bed011ef79d43
Cameron Gorrie <sastraxi@gmail.com>
2019-02-26 00:44:44 -0500

    add seed data (items)

---
+++ seed.sql
@@ -0,0 +1,15 @@
+insert into app_public.item
+  ("name", amount, category, "description", image_url)
+values (
+  'Hand-pulled pork noodles',
+  1250,
+  'chinese',
+  'Enjoy this tasty treat from the Gansu region of China.',
+  null
+), (
+  'Moo shu pork',
+  1100,
+  'chinese',
+  'Who knew tacos were invented in China? Shredded pork, cabbage, and birds-eye chiles.',
+  'https://static01.nyt.com/images/2016/09/28/dining/28CHINESE-WEB2/28CHINESE-WEB2-articleLarge.jpg'
+);
